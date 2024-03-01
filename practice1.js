672d9764a04f4ee452e6b1a03b5a160d393f9c45
Mohib <mohibullahmohim2020@gmail.com>
2024-03-02 01:23:13 +0600

    task one added

--- practice1.js
+++ practice1.js
@@ -1,6 +1,7 @@
 
-// for loop 
+// task 1
 
+// for loop 
 const oddNumber = [1,3,5,7,9];
 const makeEvenNumer = [];
 
@@ -9,3 +10,25 @@ for (const num of oddNumber) {
     makeEvenNumer.push(num2)
 }
 console.log(makeEvenNumer);
+
+// map 
+const makeEvenMap = oddNumber.map( num => num + 1) ;
+console.log(makeEvenMap);
+
+// task 2 
+
+const arrayFilter = [ 33, 50, 79, 78, 90, 101, 30 ];
+
+const newArrayFilter = [ ];
+
+for (const num of arrayFilter) {
+    if(num % 10 === 0){
+        newArrayFilter.push(num);
+    }
+}
+console.log(newArrayFilter);
+
+// filter 
+
+const mekeNewFilter = arrayFilter.filter( num => num % 10 !== 0 );
+console.log(mekeNewFilter);
