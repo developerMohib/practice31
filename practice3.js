c4791ed899b21ca7d829d707449b4dbe6a4b62db
Mohib <mohibullahmohim2020@gmail.com>
2024-03-02 02:03:58 +0600

    task three added

--- practice3.js
+++ practice3.js
@@ -5,3 +5,9 @@ const people = [
     {name : 'reena', age : 15}, 
     {name : 'sucona', age : 22}, 
 ];
+
+const peopleArray = people.map( girl => girl.age );
+console.log( peopleArray);
+
+const peopleAge = peopleArray.reduce( (accum, currenValue) => accum + currenValue ,0 );
+console.log(peopleAge);
