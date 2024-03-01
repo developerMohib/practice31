b435cc9b74178ce6c3e07253323672ebd680231d
Mohib <mohibullahmohim2020@gmail.com>
2024-03-02 00:42:43 +0600

    first commit

---
+++ practice1.js
@@ -0,0 +1,11 @@
+
+// for loop 
+
+const oddNumber = [1,3,5,7,9];
+const makeEvenNumer = [];
+
+for (const num of oddNumber) {
+    let num2 = num + 1;
+    makeEvenNumer.push(num2)
+}
+console.log(makeEvenNumer);
